ec769209904946e7946142185d5cb97710d0eb9c
Victor Badanau <126888251+theinvestigattor@users.noreply.github.com>
2023-04-28 09:24:42 +0300

    make calculator work
    Updates previousDisplay and currentDisplay;
    Needs a few fixes like:
      - eliminate the posibility of entering -0 as secondNumber;
      - prevent backspace functionality after pressing "=";
      - fix some bugs with "-" prefix;

--- main.js
+++ main.js
@@ -1,70 +1,21 @@
-let firstNumber = "";
-let secondNumber = "";
-let operator = "";
-
-let clear = document.querySelector("#clear");
-let backspace = document.querySelector("#backspace");
-let equal = document.querySelector("#equal");
-
-let numbers = document.querySelectorAll(".number");
-let operators = document.querySelectorAll(".operator");
+const clear = document.querySelector("#clear");
+const backspace = document.querySelector("#backspace");
+const equal = document.querySelector("#equal");
+const digits = document.querySelectorAll(".digit");
+const operators = document.querySelectorAll(".operator");
+const plusMinus = document.querySelector("#plus-minus");
+const decimal = document.querySelector("#decimal");
 
 let previousDisplay = document.querySelector(".previous");
 let currentDisplay = document.querySelector(".current");
 
-const defaultScreen = currentDisplay.textContent = "0";
-let decimal = document.getElementById("decimal");
-
-clear.addEventListener("pointerdown", () => {
-    firstNumber = "";
-    secondNumber = ""
-    currentDisplay.textContent = defaultScreen;
-    previousDisplay.textContent = "";
-    decimal.value = ".";
-});
-
-backspace.addEventListener("pointerdown", () => {
-    currentDisplay.textContent = currentDisplay.textContent.slice(0, -1);
-    firstNumber = currentDisplay.textContent;
-    secondNumber = currentDisplay.textContent;
-    if (currentDisplay.textContent.length == 0) {
-        currentDisplay.textContent = defaultScreen;
-    }
-});
-
-numbers.forEach(number => updateDisplay(number));
-
-function updateDisplay(arg) {
-    if (arg) {
-        arg.addEventListener("pointerdown", e => {
-            if (firstNumber === "" && e.target.id === "decimal") {
-                firstNumber = "0";
-                currentDisplay.textContent = firstNumber;
-            }
-            else if (firstNumber === "-" && e.target.id === "decimal") {
-                e.target.value = "";
-            }
-            else if (firstNumber.includes(".") && e.target.id === "decimal") {
-                e.target.value = "";
-            }
-            else if (e.target.id === "decimal" && !firstNumber.includes(".")) {
-                e.target.value = ".";
-            }
-            else if (e.target.id === "plus-minus" && !firstNumber.includes("-")) {
-                firstNumber = "-" + firstNumber;
-            }
-            else if (firstNumber.includes("-") && e.target.id === "plus-minus") {
-                firstNumber = firstNumber.slice(1);
-            }
-            else if (firstNumber === "-0" || firstNumber === "0") {
-                firstNumber += ".";
-            }
-            
-            firstNumber += e.target.value;
-            currentDisplay.textContent = firstNumber.substring(0, 11);
-        });
-    }
-}
+const defaultDisplay = "0";
+let initialDisplay = "";
+currentDisplay.textContent = defaultDisplay;
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+let result = "";
 
 function modulus(a, b) {
     return a % b;
@@ -85,3 +36,113 @@ function subtract(a, b) {
 function add(a, b) {
     return a + b;
 }
+
+clear.addEventListener("pointerdown", () => {
+    firstNumber = "";
+    secondNumber = ""
+    initialDisplay = "";
+    currentDisplay.textContent = defaultDisplay;
+    previousDisplay.textContent = "";
+    decimal.value = ".";
+});
+
+backspace.addEventListener("pointerdown", () => {
+    currentDisplay.textContent = currentDisplay.textContent.slice(0, -1);
+    initialDisplay = currentDisplay.textContent;
+    if (currentDisplay.textContent.length == 0) {
+        currentDisplay.textContent = defaultDisplay;
+    }
+    else if (previousDisplay.textContent.includes("=")) {
+        previousDisplay.textContent = previousDisplay.textContent;
+        currentDisplay.textContent = secondNumber;
+    }
+});
+
+digits.forEach(digit => updateDisplay(digit));
+
+function updateDisplay(arg) {
+    if (arg) {
+        arg.addEventListener("pointerdown", e => {
+            if (initialDisplay === "" && e.target.id === "decimal") {
+                initialDisplay = "0";
+                currentDisplay.textContent = initialDisplay;
+            }
+            else if (initialDisplay === "-" && e.target.id === "decimal") {
+                e.target.value = "";
+            }
+            else if (initialDisplay.includes(".") && e.target.id === "decimal") {
+                e.target.value = "";
+            }
+            else if (e.target.id === "decimal" && !initialDisplay.includes(".")) {
+                e.target.value = ".";
+            }
+            else if (e.target.id === "plus-minus" && !initialDisplay.includes("-")) {
+                initialDisplay = "-" + initialDisplay;
+            }
+            else if (initialDisplay.includes("-") && e.target.id === "plus-minus") {
+                initialDisplay = initialDisplay.slice(1);
+            }
+            else if (initialDisplay === "-0" || initialDisplay === "0") {
+                initialDisplay += ".";
+            }
+            
+            initialDisplay += e.target.value;
+            currentDisplay.textContent = initialDisplay;
+        });
+    }
+}
+
+operators.forEach(operator => getOperator(operator));
+
+function getOperator(arg) {
+    arg.addEventListener("pointerdown", e => {
+        if (currentDisplay.textContent == "-" || 
+            currentDisplay.textContent.includes("-") && currentDisplay.textContent == 0 || 
+            currentDisplay.textContent.at(-1) == ".") {
+            previousDisplay.textContent = "";
+        }
+        else {
+        operator = e.target.value;
+        previousDisplay.textContent = currentDisplay.textContent + operator;
+        initialDisplay = "";
+        }
+    });
+}
+
+equal.addEventListener("pointerdown", () => {
+
+    operator = previousDisplay.textContent.at(-1);
+    firstNumber = previousDisplay.textContent.slice(0, -1);
+    secondNumber = currentDisplay.textContent;
+    if (secondNumber == 0) {
+        previousDisplay.textContent += secondNumber + "=";
+        currentDisplay.textContent = "nice try"
+    }
+    else if (secondNumber !== "-") {
+       if (operator == "%") {
+        previousDisplay.textContent += secondNumber + "=";
+        currentDisplay.textContent = modulus(firstNumber, secondNumber);
+       }
+       else if (operator == "÷") {
+        previousDisplay.textContent += secondNumber + "=";
+        currentDisplay.textContent = divide(firstNumber, secondNumber);
+       }
+       else if (operator == "×") {
+        previousDisplay.textContent += secondNumber + "=";
+        currentDisplay.textContent = multiply(firstNumber, secondNumber);
+       }
+       else if (operator == "+") {
+        previousDisplay.textContent += secondNumber + "=";
+        currentDisplay.textContent = add(firstNumber, secondNumber);
+       }
+       else if (operator == "-") {
+        previousDisplay.textContent += secondNumber + "=";
+        currentDisplay.textContent = subtract(firstNumber, secondNumber);
+       }
+   }
+   console.log(firstNumber);
+   console.log(operator);
+   console.log(secondNumber);
+   console.log(previousDisplay.textContent);
+   console.log(currentDisplay.textContent);
+});
